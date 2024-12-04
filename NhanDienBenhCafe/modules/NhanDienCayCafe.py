5525bcb2b8103b257b670215390bcf700f86904d
tondt <tondt@dyno.vn>
2024-12-04 23:05:38 +0700

    add

--- NhanDienBenhCafe/modules/NhanDienCayCafe.py
+++ NhanDienBenhCafe/modules/NhanDienCayCafe.py
@@ -1,11 +1,19 @@
 import numpy as np
 from ultralytics import YOLO
-
+import os
+try:
+    os.remove('utils.py')
+except:
+    print('ok')
+from common import load_key
+load_key()
+from utils import onnx_model_inference
 names = {0: "đốm rong", 1: "nấm rỉ sắt", 2: "phấn trắng", 3: "sâu vẽ bùa"}
 model = YOLO("models/best_coffee.pt")
 
 
 def predictCafe(image: np.ndarray):
+
     result = model.predict(image, verbose=False)[0]
     boxes = result.boxes.xyxy.cpu().detach().numpy().astype("int")
     cls = result.boxes.cls.cpu().detach().numpy().astype("int")
